04fad4b2e962e6fcbf387a7c13c745e4007d1286
shubiour <sv95.shuvo@gmail.com>
2024-10-19 21:49:55 +0600

    portal used

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,16 +1,20 @@
+import { createPortal } from 'react-dom';
 export default function ResultModal({ refDialog, targetTime, remainingTime, onReset }) {
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
-  const score = Math.round(( 1 - remainingTime / (targetTime * 1000)) * 100 );
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
-  return (<dialog ref={refDialog} className="result-modal">
-    {userLost && <h2>You lost</h2>}
-    {!userLost && <h2>Your won</h2>}
-     {!userLost && <p>Your score : <strong>{score}</strong>.</p>}
-    <p>The target time was <strong>{targetTime}</strong> seconds.</p>
-    <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
-    <form method="dialog" onSubmit={onReset}>
-      <button>Close</button>
-    </form>
-  </dialog>);
+  return createPortal(
+    <dialog ref={refDialog} className="result-modal">
+      {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your won</h2>}
+      {!userLost && <p>Your score : <strong>{score}</strong>.</p>}
+      <p>The target time was <strong>{targetTime}</strong> seconds.</p>
+      <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+      <form method="dialog" onSubmit={onReset}>
+        <button>Close</button>
+      </form>
+    </dialog>, 
+    document.getElementById('modal')
+  );
 }

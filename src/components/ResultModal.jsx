aca4d136faf2c26cf9934b21b6d0c728bb2833ca
shubiour <sv95.shuvo@gmail.com>
2024-10-19 21:39:27 +0600

    bug-showing wining resut

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,9 +1,12 @@
 export default function ResultModal({ refDialog, targetTime, remainingTime, onReset }) {
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round(( 1 - remainingTime / (targetTime * 1000)) * 100 );
 
   return (<dialog ref={refDialog} className="result-modal">
     {userLost && <h2>You lost</h2>}
+    {!userLost && <h2>Your won</h2>}
+     {!userLost && <p>Your score : <strong>{score}</strong>.</p>}
     <p>The target time was <strong>{targetTime}</strong> seconds.</p>
     <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
     <form method="dialog" onSubmit={onReset}>

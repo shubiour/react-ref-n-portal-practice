54e92a1a67510d1516dcd34f96d559285e24a8fe
shubiour <sv95.shuvo@gmail.com>
2024-10-19 21:33:07 +0600

    imp-modal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,9 +1,12 @@
-export default function ResultModal({ refDialog, result, targetTime }) {
+export default function ResultModal({ refDialog, targetTime, remainingTime, onReset }) {
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (<dialog ref={refDialog} className="result-modal">
-    <h2>You {result}</h2>
+    {userLost && <h2>You lost</h2>}
     <p>The target time was <strong>{targetTime}</strong> seconds.</p>
-    <p>You stopped the timer with <strong>X seconds left.</strong></p>
-    <form method="dialog">
+    <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+    <form method="dialog" onSubmit={onReset}>
       <button>Close</button>
     </form>
   </dialog>);
